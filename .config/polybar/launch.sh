4421280db31eb4194e2116cdb87760e872b6bc9d
Rod <miguelravila@outlook.com>
2020-09-26 23:04:50 -0500

    Polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
-
 killall -q polybar
-
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar work -c ~/.config/polybar/config.ini &
+polybar primary -c ~/.config/polybar/workspace.ini &
+polybar primary -c ~/.config/polybar/system.ini &
