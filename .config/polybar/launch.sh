582f0760daf9f4268eba2ab6320ed40fb97584e3
Miguel R. Ávika <miguelravila@outlook.com>
2020-10-25 21:13:41 -0600

    Polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,4 +3,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 polybar primary -c ~/.config/polybar/workspace.ini &
+ln -s /tmp/polybar_mqueue.$! /tmp/ipc-mybar1
+
 polybar primary -c ~/.config/polybar/system.ini &
+ln -s /tmp/polybar_mqueue.$! /tmp/ipc-mybar2
