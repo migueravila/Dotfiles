b63ba803abc099482c62adda7668c3f6fdc8f13c
Miguel R. Ávila <miguelravila@outlook.com>
2020-12-13 19:12:47 -0600

    Bars and picom

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env sh
+#   ___     _      _              
+#  | _ \___| |_  _| |__  __ _ _ _ 
+#  |  _/ _ \ | || | '_ \/ _` | '_|
+#  |_| \___/_|\_, |_.__/\__,_|_|  
+#             |__/                
+#  - Miguel R. Ávila
+
+
+# Kill actual polybar
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Launch Workspaces Bar
 polybar primary -c ~/.config/polybar/workspace.ini &
 ln -s /tmp/polybar_mqueue.$! /tmp/ipc-mybar1
 
+# Launch System bar
 polybar primary -c ~/.config/polybar/system.ini &
 ln -s /tmp/polybar_mqueue.$! /tmp/ipc-mybar2
